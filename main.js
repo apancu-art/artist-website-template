a7e7bf108fb25d4fa2914b9942e11c0c3c3dac9c
andrei.pancu <andrei.pancu@taske.com>
2025-09-01 08:07:32 -0400

    more styling

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     let homeLink = document.querySelector('.artist-name');
-    let navLinks = document.querySelectorAll('.navbar-right a');
+    let navLinks = document.querySelectorAll('.navbar a');
     let sections = document.querySelectorAll('main > section');
 
     function showSection(hash) {
         if (hash) {
             document.body.classList.remove('bg-image');
-        }        
+            for (let link of navLinks) {
+                link.style.fontWeight = '';
+            }
+        } else {
+            for (let link of navLinks) {
+                link.style.fontWeight = link.classList.contains('artist-name') ? 500 : 400;
+            }
+        }       
         sections.forEach(section => {
             section.hidden = (hash !== '#' + section.id);
         });
@@ -23,8 +30,7 @@ document.addEventListener("DOMContentLoaded", function () {
         link.addEventListener('click', function (e) {
             e.preventDefault();
             for (let link of this.parentNode.children) {
-                link.style.color = link == this ? 'black' : '';
-                link.style.fontWeight = link == this ? 300 : '';
+                link.style.fontWeight = link == this ? 400 : '';
             }
             let targetHash = this.getAttribute('href');
             window.location.hash = targetHash;
